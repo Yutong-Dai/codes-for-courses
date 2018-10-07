fdf52acdd613b7832b245d17479896f910dfbf0d
Roth <rothdyt@gmail.com>
2018-10-06 23:34:10 -0500

    updated

---
+++ Python_IE534/hw5/image_ranking.py
@@ -0,0 +1,8 @@
+'''
+File: image_ranking.py
+Author: Yutong Dai (rothdyt@gmail.com)
+File Created: Saturday, 2018-10-06 11:41
+Last Modified: Saturday, 2018-10-06 22:19
+--------------------------------------------
+Desscription:
+'''
