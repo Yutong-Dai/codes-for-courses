f3328751af9a57796e54b649445e7b2b6d61d36c
Roth <rothdyt@gmail.com>
2018-11-22 18:58:34 -0600

    updated

--- Python_IE534/Fina_Project/generate_vocab_dict.py
+++ Python_IE534/Fina_Project/generate_vocab_dict.py
@@ -6,28 +6,10 @@ Last Modified: Saturday, 2018-11-17 14:39
 --------------------------------------------
 Desscription:
 '''
-import nltk
-import pickle
-from collections import Counter
-from pycocotools.coco import COCO
-import logging
-import numpy as np
-
-log_level = logging.INFO
-logger = logging.getLogger()
-logger.setLevel(log_level)
-handler = logging.FileHandler("data-preprocess.log")
-handler.setLevel(log_level)
-formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-json = "../../../../Desktop/annotations/captions_val2017.json"
-portion = 0.95
-save_path = "./vocab.pkl"
-
 
 # Reference <https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/03-advanced/image_captioning/build_vocab.py#L8>
+
+
 class Vocabulary(object):
     """Simple vocabulary wrapper."""
 
@@ -44,50 +26,70 @@ class Vocabulary(object):
 
     def __call__(self, word):
         if not word in self.word2idx:
-            return self.word2idx['<unk>']
+            return self.word2idx['<<unknown>>']
         return self.word2idx[word]
 
     def __len__(self):
         return len(self.word2idx)
 
 
-# construct coco instance
-# Reference: <https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/03-advanced/image_captioning/build_vocab.py#L31>
-coco = COCO(json)
-ids = coco.anns.keys()
-counter = Counter()
-for i, id in enumerate(ids):
-    caption = str(coco.anns[id]['caption'])
-    tokens = nltk.tokenize.word_tokenize(caption.lower())
-    counter.update(tokens)
-    if (i+1) % 5000 == 0:
-        print("Tokenization Process: {0:.2f}%.".format((i+1)*100/len(ids)))
-        logger.info("Tokenization Process: {0:.2f}%.".format((i+1)*100/len(ids)))
-# Keep the most frequently appeared words
-counts = []
-for _, count in counter.items():
-    counts.append(count)
-counts.sort(reverse=True)
-cum_ratio = np.cumsum(counts) / np.sum(counts)
-threshold = counts[np.argmax(cum_ratio > portion)]
-words = []
-for word, count in counter.items():
-    if count >= threshold:
-        words.append(word)
-words.sort()
-vocab = Vocabulary()
-vocab.add_word('<<start>>')
-vocab.add_word('<<end>>')
-vocab.add_word('<<unknown>>')
-# for padding purpose
-vocab.add_word('<<padding>>')
-# Add the words to the vocabulary.
-for word in words:
-    vocab.add_word(word)
+if __name__ == "__main__":
+    import nltk
+    from collections import Counter
+    from pycocotools.coco import COCO
+    import logging
+    import numpy as np
+    import pickle
 
-with open(save_path, 'wb') as f:
-    pickle.dump(vocab, f)
-print("Total vocabulary size: {}".format(len(vocab)))
-logger.info("Total vocabulary size: {}".format(len(vocab)))
-print("Saved the vocabulary wrapper to '{}'".format(save_path))
-logger.info("Saved the vocabulary wrapper to '{}'".format(save_path))
+    log_level = logging.INFO
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    handler = logging.FileHandler("data-preprocess.log")
+    handler.setLevel(log_level)
+    formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    json = "../../../../Desktop/annotations/captions_val2017.json"
+    portion = 0.95
+    save_path = "./vocab.pkl"
+    # construct coco instance
+    # Reference: <https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/03-advanced/image_captioning/build_vocab.py#L31>
+    coco = COCO(json)
+    ids = coco.anns.keys()
+    counter = Counter()
+    for i, id in enumerate(ids):
+        caption = str(coco.anns[id]['caption'])
+        tokens = nltk.tokenize.word_tokenize(caption.lower())
+        counter.update(tokens)
+        if (i+1) % 5000 == 0:
+            print("Tokenization Process: {0:.2f}%.".format((i+1)*100/len(ids)))
+            logger.info("Tokenization Process: {0:.2f}%.".format((i+1)*100/len(ids)))
+    # Keep the most frequently appeared words
+    counts = []
+    for _, count in counter.items():
+        counts.append(count)
+    counts.sort(reverse=True)
+    cum_ratio = np.cumsum(counts) / np.sum(counts)
+    threshold = counts[np.argmax(cum_ratio > portion)]
+    words = []
+    for word, count in counter.items():
+        if count >= threshold:
+            words.append(word)
+    words.sort()
+    vocab = Vocabulary()
+    # for padding purpose
+    vocab.add_word('<<padding>>')
+    vocab.add_word('<<start>>')
+    vocab.add_word('<<end>>')
+    vocab.add_word('<<unknown>>')
+    # Add the words to the vocabulary.
+    for word in words:
+        vocab.add_word(word)
+
+    with open(save_path, 'wb') as f:
+        pickle.dump(vocab, f)
+    print("Total vocabulary size: {}".format(len(vocab)))
+    logger.info("Total vocabulary size: {}".format(len(vocab)))
+    print("Saved the vocabulary wrapper to '{}'".format(save_path))
+    logger.info("Saved the vocabulary wrapper to '{}'".format(save_path))
---
+++ Python_IE534/Fina_Project/train.py
@@ -0,0 +1,88 @@
+'''
+File: train.py
+Author: Yutong Dai (rothdyt@gmail.com)
+File Created: Sunday, 2018-11-18 14:42
+Last Modified: Sunday, 2018-11-18 14:43
+--------------------------------------------
+Desscription:
+'''
+
+
+import argparse
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+import pickle
+from data_loader import CocoDataset, coco_batch
+from generate_vocab_dict import Vocabulary
+from model import Encoder, Decoder
+from torch.nn.utils.rnn import pack_padded_sequence
+from torchvision import transforms
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def main(args):
+    # Create model directory
+    if not os.path.exists(args.model_path):
+        os.makedirs(args.model_path)
+
+    # Image preprocessing, normalization for the pretrained resnet
+    transform = transforms.Compose([
+        transforms.RandomCrop(args.crop_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
+
+    # Load vocabulary wrapper
+    with open(args.vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
+
+    # Build data loader
+    data_loader = get_loader(args.image_dir, args.caption_path, vocab,
+                             transform, args.batch_size,
+                             shuffle=True, num_workers=args.num_workers)
+
+    # Build the models
+    encoder = EncoderCNN(args.embed_size).to(device)
+    decoder = DecoderRNN(args.embed_size, args.hidden_size, len(vocab), args.num_layers).to(device)
+
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    params = list(decoder.parameters()) + list(encoder.linear.parameters()) + list(encoder.bn.parameters())
+    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+
+    # Train the models
+    total_step = len(data_loader)
+    for epoch in range(args.num_epochs):
+        for i, (images, captions, lengths) in enumerate(data_loader):
+
+            # Set mini-batch dataset
+            images = images.to(device)
+            captions = captions.to(device)
+            targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+
+            # Forward, backward and optimize
+            features = encoder(images)
+            outputs = decoder(features, captions, lengths)
+            loss = criterion(outputs, targets)
+            decoder.zero_grad()
+            encoder.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Print log info
+            if i % args.log_step == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
+                      .format(epoch, args.num_epochs, i, total_step, loss.item(), np.exp(loss.item())))
+
+            # Save the model checkpoints
+            if (i+1) % args.save_step == 0:
+                torch.save(decoder.state_dict(), os.path.join(
+                    args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+                torch.save(encoder.state_dict(), os.path.join(
+                    args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
